edb6abcb62752c01d6170b62855c1640a67fe647
akash-suresh <akashsuresh36@gmail.com>
2016-02-22 15:44:28 +0530

    Made hououse 3d and shearing and reflection

--- house/house_code.c
+++ house/house_code.c
@@ -62,10 +62,9 @@ void drawcircle(x,y,P,Q,color){
 	putpixel(x-Q,y-P,color);
 }
 
-void house(int x, int y, float scale, float angle, int r,int s){
+void house(int x, int y, float scale, float angle, int r,float sh){
 	int color=RED;
-	
-	drawLine(x,y,x+200,y,color,scale,angle,s);
+/*	drawLine(x,y,x+200,y,color,scale,angle,s);
 	drawLine(x,y-r*100,x+200,y-r*100,color,scale,angle,s);
 	if(r==1){
 		drawLine(x,y-r*100,x,y,color,scale,angle,s);
@@ -84,15 +83,53 @@ void house(int x, int y, float scale, float angle, int r,int s){
 
 	drawLine(x+40,y-r*150,x+160,y-r*150,color,scale,angle,s);
 	drawLine(x+160,y-r*150,x+200,y-r*100,color,scale,angle,s);
+*/
+
+	int points1[]={x,y,x+100,y,x+100+100*sh,y-r*100,x+100*sh,y-r*100,x,y};
+	int points2[]={x+30,y-r*30,x+130,y-r*30,x+130+100*sh,y-r*130,x+30+100*sh,y-r*130,x+30,y-30};
+	int points3[]={x,y,x+100*sh,y-r*100,x+30+100*sh,y-r*130,x+30,y-r*30,x,y};
+	int points4[]={x+100,y,x+100+100*sh,y-r*100,x+130+100*sh,y-r*130,x+130,y-r*30,x+100,y};
+	int points5[]={x+100*sh,y-r*100,x+50+170*sh,y-r*170,x+100+100*sh,y-r*100,x+100*sh,y-r*100};
+	int points6[]={x+30+100*sh,y-r*130,x+50+170*sh,y-r*170,x+100*sh,y-r*100,x+30+100*sh,y-r*130};
+	int points7[]={x+100+100*sh,y-r*100,x+50+170*sh,y-r*170,x+130+100*sh,y-r*130,x+100+100*sh,y-r*100};
+	
+	
+	setlinestyle(0,1,1);
+	//setcolor(RED);
+	drawpoly(5, points1);
+	drawpoly(5, points4);
+	drawpoly(4, points5);
+	drawpoly(4, points7);
+
+
+	setlinestyle(3,2,1);
+	//setcolor(BLUE);
+	drawpoly(5, points2);
+	//setcolor(MAGENTA);
+	drawpoly(5, points3);
+	drawpoly(4, points6);
 
 }
 int main(){
-	int gd = DETECT, gm;
+	int gd = DETECT, gm,choice;
 	initgraph(&gd,&gm,NULL);
 
 	int x = 100, y= 250,i,angle;
+	printf("Menu : 1. Reflect 2.Shear\n");
+	scanf("%d",&choice);
+	switch(choice){
+		case 1:
+			house(x,y,1,0,1,0);
+			house(x,y,1,0,-1,0);
+			break;
 
-	house(x,y,1,0,1,1);
+		case 2:house(x,y,1,0,1,0.3);
+			break;
+
+		case 3:house(x,y,1,0,1,0);
+			break;
+	}
+//	house(x,y,1,0,-1);
 	/*
 	//code for moving cycle
 	for(i=0;i<100;i++){
